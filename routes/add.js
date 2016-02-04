a517f2f96ab2cf95412875e147d46bbf3e1c627f
calanimagebeta <atpachec@ucsd.edu>
2016-02-04 12:22:00 -0800

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,12 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var newFriend = {
+		'name': req.query.name,
+		'description': req.query.description,
+		'imageURL': "http://lorempixel.com/400/400/people"
+	}
+	res.render('add', newFriend);
+	console.log(newFriend);
+	data["friends"].push(newFriend);
  }
